e6facbe801e7173844d1b2a0397029e421d24f11
Callum Watkins <callum@callumwatkins.com>
2022-12-19 22:36:18 +0000

    Add global CSS to fix Nuxt root div style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/css/_global.scss" as *;',
+        },
+      },
+    },
+  },
 })

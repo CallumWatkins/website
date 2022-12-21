f5a7025c2e1701472e45003c56205d9f16bac244
Callum Watkins <callum@callumwatkins.com>
2022-12-21 22:55:20 +0000

    Create GenericFlair component for code reuse

--- components/GitHubFlair.vue
+++ components/GitHubFlair.vue
@@ -1,33 +1,31 @@
 <template>
-  <a href="https://github.com/CallumWatkins" target="_blank" rel="noopener me">
-    <div class="flair">
-      <div class="flair__grid">
-        <div class="flair__profile-picture">
-          <img src="@/assets/images/logos/CW_Logo.svg">
+  <GenericFlair
+    profileName="Callum Watkins"
+    :profilePicture="CW_Logo"
+    :logo="GitHubLogo"
+    href="https://github.com/CallumWatkins">
+    <template v-slot:details>
+      <template v-if="data">
+        <div class="flair__contributions">
+          <div><FontAwesomeIcon icon="fa-solid fa-code-commit" size="xs" /> {{ data.data.user.contributionsCollection.contributionCalendar.totalContributions.toLocaleString() }} contributions in the last year</div>
         </div>
-        <div class="flair__service-logo">
-          <img src="@/assets/images/github-mark-white.svg" />
+        <div class="flair__questions-answers">
+          <div><span>{{ data.data.user.repositories.totalCount }}</span> Repositories</div>
+          <div><span>{{ data.data.user.gists.totalCount }}</span> Gists</div>
+          <div><span>{{ new Date(Date.now() - Date.parse(data.data.user.createdAt)).getUTCFullYear() - 1970 }}</span> Years</div>
         </div>
-        <div class="flair__name">Callum Watkins</div>
-        <div class="flair__details" v-if="data">
-          <div class="flair__contributions">
-            <div><FontAwesomeIcon icon="fa-solid fa-code-commit" size="xs" /> {{ data.data.user.contributionsCollection.contributionCalendar.totalContributions.toLocaleString() }} contributions in the last year</div>
-          </div>
-          <div class="flair__questions-answers">
-            <div><span>{{ data.data.user.repositories.totalCount }}</span> Repositories</div>
-            <div><span>{{ data.data.user.gists.totalCount }}</span> Gists</div>
-            <div><span>{{ new Date(Date.now() - Date.parse(data.data.user.createdAt)).getUTCFullYear() - 1970 }}</span> Years</div>
-          </div>
-        </div>
-        <div class="flair__details" v-else>
-          Loading...
-        </div>
-      </div>
-    </div>
-  </a>
+      </template>
+      <template v-else>
+        Loading...
+      </template>
+    </template>
+  </GenericFlair>
 </template>
 
 <script setup lang="ts">
+import CW_Logo from '@/assets/images/logos/CW_Logo.svg';
+import GitHubLogo from '@/assets/images/github-mark-white.svg';
+
 const { data } = useLazyFetch<GHResponse>(
   '/api/github',
   {
@@ -68,63 +66,7 @@ interface GHGists {
 </script>
 
 <style lang="scss" scoped>
-a:focus, a:hover {
-  .flair {
-    transform: translateY(-4px);
-    box-shadow: 0px 5px 5px 1px rgba(150,150,150,1);
-  }
-}
-
 .flair {
-  padding: 1rem;
-  background-color: #2d2d2d;
-  transition: all 0.25s ease-out;
-  text-align: left;
-
-  &__grid {
-    color: white;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI Adjusted", "Segoe UI", "Liberation Sans", sans-serif;
-    width: 31rem;
-    height: 6rem;
-    display: grid;
-    grid-template-columns: 6rem 1fr 2.5rem;
-    grid-template-rows: 2.5rem 1fr;
-    grid-auto-flow: row;
-    grid-template-areas:
-      "profile-picture name service-logo"
-      "profile-picture details details";
-  }
-
-  &__profile-picture {
-    grid-area: profile-picture;
-  }
-
-  &__service-logo {
-    grid-area: service-logo;
-    display: flex;
-    justify-content: flex-end;
-  }
-
-  &__name {
-    grid-area: name;
-    display: flex;
-    font-weight: bold;
-    align-items: center;
-    padding-left: 1rem;
-  }
-
-  &__details {
-    grid-area: details;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    gap: 0.2rem;
-    padding-left: 1rem;
-    line-height: 1.6rem;
-    font-size: 1.4rem;
-  }
-
   &__contributions {
     width: 100%;
     text-align: left;
--- components/StackOverflowFlair.vue
+++ components/StackOverflowFlair.vue
@@ -1,34 +1,32 @@
 <template>
-  <a href="https://stackoverflow.com/users/4415734/callum-watkins" target="_blank" rel="noopener me">
-    <div class="flair">
-      <div class="flair__grid">
-        <div class="flair__profile-picture">
-          <img src="@/assets/images/logos/CW_Logo.svg">
+  <GenericFlair
+    profileName="Callum Watkins"
+    :profilePicture="CW_Logo"
+    :logo="StackOverflowLogo"
+    href="https://stackoverflow.com/users/4415734/callum-watkins">
+    <template v-slot:details>
+      <template v-if="user">
+        <div class="flair__rep-badges">
+          <div><FontAwesomeIcon icon="fa-solid fa-star" size="2xs" /> {{ user.reputation.toLocaleString() }}</div>
+          <div><span class="gold"><FontAwesomeIcon icon="fa-solid fa-circle" size="2xs" /> {{ user.badge_counts.gold }}</span> <span class="silver"><FontAwesomeIcon icon="fa-solid fa-circle" size="2xs" /> {{ user.badge_counts.silver }}</span> <span class="bronze"><FontAwesomeIcon icon="fa-solid fa-circle" size="2xs" /> {{ user.badge_counts.bronze }}</span></div>
         </div>
-        <div class="flair__service-logo">
-          <img src="@/assets/images/stack-overflow.svg" />
+        <div class="flair__questions-answers">
+          <div><span>{{ user.question_count }}</span> Questions</div>
+          <div><span>{{ user.answer_count }}</span> Answers</div>
+          <div><span>{{ new Date(Date.now() - user.creation_date * 1000).getUTCFullYear() - 1970 }}</span> Years</div>
         </div>
-        <div class="flair__name">Callum Watkins</div>
-        <div class="flair__details" v-if="user">
-          <div class="flair__rep-badges">
-            <div><FontAwesomeIcon icon="fa-solid fa-star" size="2xs" /> {{ user.reputation.toLocaleString() }}</div>
-            <div><span class="gold"><FontAwesomeIcon icon="fa-solid fa-circle" size="2xs" /> {{ user.badge_counts.gold }}</span> <span class="silver"><FontAwesomeIcon icon="fa-solid fa-circle" size="2xs" /> {{ user.badge_counts.silver }}</span> <span class="bronze"><FontAwesomeIcon icon="fa-solid fa-circle" size="2xs" /> {{ user.badge_counts.bronze }}</span></div>
-          </div>
-          <div class="flair__questions-answers">
-            <div><span>{{ user.question_count }}</span> Questions</div>
-            <div><span>{{ user.answer_count }}</span> Answers</div>
-            <div><span>{{ new Date(Date.now() - user.creation_date * 1000).getUTCFullYear() - 1970 }}</span> Years</div>
-          </div>
-        </div>
-        <div class="flair__details" v-else>
-          Loading...
-        </div>
-      </div>
-    </div>
-  </a>
+      </template>
+      <template v-else>
+        Loading...
+      </template>
+    </template>
+  </GenericFlair>
 </template>
 
 <script setup lang="ts">
+import CW_Logo from '@/assets/images/logos/CW_Logo.svg';
+import StackOverflowLogo from '@/assets/images/stack-overflow.svg';
+
 const userId = '4415734';
 
 const { data } = useLazyFetch<SOUsers>(
@@ -61,63 +59,7 @@ interface SOUserBadgeCounts {
 </script>
 
 <style lang="scss" scoped>
-a:focus, a:hover {
-  .flair {
-    transform: translateY(-4px);
-    box-shadow: 0px 5px 5px 1px rgba(150,150,150,1);
-  }
-}
-
 .flair {
-  padding: 1rem;
-  background-color: #2d2d2d;
-  transition: all 0.25s ease-out;
-  text-align: left;
-
-  &__grid {
-    color: white;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI Adjusted", "Segoe UI", "Liberation Sans", sans-serif;
-    width: 31rem;
-    height: 6rem;
-    display: grid;
-    grid-template-columns: 6rem 1fr 2.5rem;
-    grid-template-rows: 2.5rem 1fr;
-    grid-auto-flow: row;
-    grid-template-areas:
-      "profile-picture name service-logo"
-      "profile-picture details .";
-  }
-
-  &__profile-picture {
-    grid-area: profile-picture;
-  }
-
-  &__service-logo {
-    grid-area: service-logo;
-    display: flex;
-    justify-content: flex-end;
-  }
-
-  &__name {
-    grid-area: name;
-    display: flex;
-    font-weight: bold;
-    align-items: center;
-    padding-left: 1rem;
-  }
-
-  &__details {
-    grid-area: details;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    gap: 0.2rem;
-    padding-left: 1rem;
-    line-height: 1.6rem;
-    font-size: 1.4rem;
-  }
-
   &__rep-badges {
     width: 100%;
     display: flex;

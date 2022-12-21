9976f25e4ce8d01648e9e05d19ab9db0e8eee250
Callum Watkins <callum@callumwatkins.com>
2022-12-21 01:13:30 +0000

    Create GitHub and StackOverflow flair components

--- components/OnlineSection.vue
+++ components/OnlineSection.vue
@@ -10,56 +10,13 @@
       <div class="col-six tab-full">
         <h3>Stack Overflow</h3>
         <p>I am a member of the Stack Overflow Q&amp;A community, contributing questions and answers to the wide community of professional and enthusiast programmers.</p>
-        <a href="https://stackoverflow.com/users/4415734/callum-watkins" target="_blank" rel="noopener me">
-          <img src="https://stackoverflow.com/users/flair/4415734.png?theme=clean" class="stroke link" width="208" height="58" alt="profile for Callum Watkins at Stack Overflow, Q&amp;A for professional and enthusiast programmers" title="profile for Callum Watkins at Stack Overflow, Q&amp;A for professional and enthusiast programmers">
-        </a>
+        <StackOverflowFlair />
       </div>
       <div class="col-six tab-full">
         <h3>GitHub &amp; Gist</h3>
         <p>GitHub hosts my public code resositories and Gists. Gists are a handy way of sharing code; there's everything from snippets to whole projects, available for public use.</p>
-        <a href="https://github.com/CallumWatkins" target="_blank" rel="noopener me">
-          <div class="github-flair">
-            <svg title="GitHub Flair" viewBox="0 0 250 250">
-              <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
-              <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor"></path>
-            </svg>
-            <div class="avatar">
-              <img src="@/assets/images/logos/CW_Logo.svg" alt="Profile Avatar">
-            </div>
-            <div class="info">
-              <div class="name">CallumWatkins</div>
-              <div class="meta" v-if="gitHubFlairData">
-                <span title="Total Public Repositories">
-                  <svg viewBox="0 0 12 16">
-                    <path fill-rule="evenodd" d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path>
-                  </svg> {{ gitHubFlairData.public_repos}}&nbsp;&nbsp;
-                </span>
-                <span title="Total Public Gists">
-                  <svg viewBox="0 0 12 16">
-                    <path fill-rule="evenodd" d="M7.5 5L10 7.5 7.5 10l-.75-.75L8.5 7.5 6.75 5.75 7.5 5zm-3 0L2 7.5 4.5 10l.75-.75L3.5 7.5l1.75-1.75L4.5 5zM0 13V2c0-.55.45-1 1-1h10c.55 0 1 .45 1 1v11c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1zm1 0h10V2H1v11z"></path>
-                  </svg> {{ gitHubFlairData.public_gists }}
-                </span>
-              </div>
-            </div>
-          </div>
-        </a>
+        <GitHubFlair />
       </div>
     </div>
   </section>
 </template>
-
-<script setup lang="ts">
-import { Endpoints as GitHubEndpoints } from "@octokit/types";
-
-const { data: gitHubFlairData } = useLazyFetch<GitHubEndpoints['GET /users/{username}']['response']['data']>(
-  'https://api.github.com/users/CallumWatkins',
-  {
-    headers: {
-      'Accept': 'application/vnd.github+json',
-      // 'X-GitHub-Api-Version': '2022-11-28', // TODO: Reintroduce this header once GitHub has fixed their CORS https://github.com/community/community/discussions/40619
-    },
-    pick: [ 'public_repos', 'public_gists' ],
-    server: false,
-  },
-);
-</script>
---
+++ components/StackOverflowFlair.vue
@@ -0,0 +1,5 @@
+<template>
+  <a href="https://stackoverflow.com/users/4415734/callum-watkins" target="_blank" rel="noopener me">
+    <img src="https://stackoverflow.com/users/flair/4415734.png?theme=clean" class="stroke link" width="208" height="58" alt="profile for Callum Watkins at Stack Overflow, Q&amp;A for professional and enthusiast programmers" title="profile for Callum Watkins at Stack Overflow, Q&amp;A for professional and enthusiast programmers">
+  </a>
+</template>

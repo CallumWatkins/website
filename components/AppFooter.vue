518147580e9c31794e2104046f673af9b9528499
Callum Watkins <callum@callumwatkins.com>
2022-12-21 01:17:17 +0000

    Create SocialLinks component

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,16 +2,7 @@
   <footer>
     <div class="row">
       <div class="col-six tab-full pull-right">
-        <div class="social">
-          <div><a href="mailto:contact@callumwatkins.com" title="Email me"><FontAwesomeIcon icon="fa-regular fa-envelope" fixedWidth /></a></div><!--
-       --><div><a href="https://t.me/CallumWatkins" target="_blank" rel="noopener me" title="Message me on Telegram"><FontAwesomeIcon icon="fa-brands fa-telegram" fixedWidth /></a></div><!--
-       --><div><a href="https://keybase.io/CallumWatkins" target="_blank" rel="noopener me" title="Find me on Keybase"><FontAwesomeIcon icon="fa-brands fa-keybase" fixedWidth /></a></div><!--
-       --><div><a href="https://twitter.com/_CallumWatkins" target="_blank" rel="noopener me" title="Find me on Twitter"><FontAwesomeIcon icon="fa-brands fa-twitter" fixedWidth /></a></div><!--
-       --><div><a href="https://github.com/CallumWatkins" target="_blank" rel="noopener me" title="Find me on GitHub"><FontAwesomeIcon icon="fa-brands fa-github" fixedWidth /></a></div><!--
-       --><div><a href="https://stackoverflow.com/users/4415734/callum-watkins" target="_blank" rel="noopener me" title="Find me on Stack Overflow"><FontAwesomeIcon icon="fa-brands fa-stack-overflow" fixedWidth /></a></div><!--
-       --><div><a href="https://www.linkedin.com/in/CallumWatkins" target="_blank" rel="noopener me" title="Find me on LinkedIn"><FontAwesomeIcon icon="fa-brands fa-linkedin-in" fixedWidth /></a></div><!--
-       --><div><a href="https://www.paypal.me/CallumWatkins" target="_blank" rel="noopener me" title="Send money with PayPal"><FontAwesomeIcon icon="fa-brands fa-paypal" fixedWidth /></a></div>
-        </div>
+        <SocialLinks />
       </div>
       <div class="col-six tab-full">
         <div class="copyright">

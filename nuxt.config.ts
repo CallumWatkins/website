53f30ae82cb97c898a03dcd02cc11faed0df6c1e
Callum Watkins <callum@callumwatkins.com>
2023-01-03 23:12:24 +0000

    Fix FontAwesome errors by adding to transpile list

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: [
+      '@fortawesome/fontawesome-svg-core',
+      '@fortawesome/free-solid-svg-icons',
+      '@fortawesome/free-regular-svg-icons',
+      '@fortawesome/free-brands-svg-icons',
+      '@fortawesome/vue-fontawesome',
+    ],
+  },
   css: [
     '@fortawesome/fontawesome-svg-core/styles.css',
   ],

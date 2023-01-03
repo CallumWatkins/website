e597f6ea468443f4d8aa5f50296528d4b0dc83ff
Callum Watkins <callum@callumwatkins.com>
2023-01-03 21:39:54 +0000

    Generate social links from data file

--- components/SocialLinks.vue
+++ components/SocialLinks.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="social">
-    <div><NuxtLink href="mailto:contact@callumwatkins.com" title="Email me"><FontAwesomeIcon icon="fa-regular fa-envelope" fixedWidth /></NuxtLink></div>
-    <div><NuxtLink href="https://t.me/CallumWatkins" target="_blank" rel="noopener noreferrer me" title="Message me on Telegram"><FontAwesomeIcon icon="fa-brands fa-telegram" fixedWidth /></NuxtLink></div>
-    <div><NuxtLink href="https://keybase.io/CallumWatkins" target="_blank" rel="noopener noreferrer me" title="Find me on Keybase"><FontAwesomeIcon icon="fa-brands fa-keybase" fixedWidth /></NuxtLink></div>
-    <div><NuxtLink href="https://twitter.com/_CallumWatkins" target="_blank" rel="noopener noreferrer me" title="Find me on Twitter"><FontAwesomeIcon icon="fa-brands fa-twitter" fixedWidth /></NuxtLink></div>
-    <div><NuxtLink href="https://github.com/CallumWatkins" target="_blank" rel="noopener noreferrer me" title="Find me on GitHub"><FontAwesomeIcon icon="fa-brands fa-github" fixedWidth /></NuxtLink></div>
-    <div><NuxtLink href="https://stackoverflow.com/users/4415734/callum-watkins" target="_blank" rel="noopener noreferrer me" title="Find me on Stack Overflow"><FontAwesomeIcon icon="fa-brands fa-stack-overflow" fixedWidth /></NuxtLink></div>
-    <div><NuxtLink href="https://www.linkedin.com/in/CallumWatkins" target="_blank" rel="noopener noreferrer me" title="Find me on LinkedIn"><FontAwesomeIcon icon="fa-brands fa-linkedin-in" fixedWidth /></NuxtLink></div>
-    <div><NuxtLink href="https://www.paypal.me/CallumWatkins" target="_blank" rel="noopener noreferrer me" title="Send money with PayPal"><FontAwesomeIcon icon="fa-brands fa-paypal" fixedWidth /></NuxtLink></div>
+    <div v-for="s in Social">
+      <NuxtLink :href="s.url" target="_blank" rel="noopener noreferrer me" :title="s.title">
+        <FontAwesomeIcon :icon="s.icon" fixedWidth />
+      </NuxtLink>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import Social from "@/content/social.json";
+</script>

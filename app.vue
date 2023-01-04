5cdbebf3c6eefdf284b06dae879930c81d452c5c
Callum Watkins <callum@callumwatkins.com>
2023-01-04 22:58:37 +0000

    Add lang attribute to html tag

--- app.vue
+++ app.vue
@@ -6,6 +6,9 @@
 
 <script setup lang="ts">
 useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
   script: [
     { src: "https://static.cloudflareinsights.com/beacon.min.js", defer: true, "data-cf-beacon": '{"token": "6b20566368134fdab84a60c9b25700ff"}' }, // Cloudflare Web Analytics
   ],

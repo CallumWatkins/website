899089568ce677c90fcecb3e5e7e6220c5b37f93
Callum Watkins <callum@callumwatkins.com>
2023-01-04 20:40:30 +0000

    Add Cloudflare Web Analytics

--- app.vue
+++ app.vue
@@ -3,3 +3,11 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useHead({
+  script: [
+    { src: "https://static.cloudflareinsights.com/beacon.min.js", defer: true, "data-cf-beacon": '{"token": "6b20566368134fdab84a60c9b25700ff"}' }, // Cloudflare Web Analytics
+  ],
+});
+</script>

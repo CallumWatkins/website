9395dbc0be78f8a062115a5e579e1d1c01c79287
Callum Watkins <callum@callumwatkins.com>
2023-04-11 14:45:52 +0100

    Fix ESLint issue in fontawesome.ts

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,4 +1,5 @@
 import { library, config } from "@fortawesome/fontawesome-svg-core";
+// eslint-disable-next-line import/named
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import {
